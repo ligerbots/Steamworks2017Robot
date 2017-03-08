8a568d5885a971c91680dab14f9b839598351cc1
Erik Uhlmann <erikuhlmann@gmail.com>
2017-03-07 20:53:23 -0500

    Make turncommand not wildly rotate

--- src/org/ligerbots/steamworks/commands/TurnCommand.java
+++ src/org/ligerbots/steamworks/commands/TurnCommand.java
@@ -40,21 +40,26 @@ public class TurnCommand extends AccessibleCommand {
   public TurnCommand(double offsetDegrees) {
     super("TurnCommand_" + offsetDegrees);
     requires(Robot.driveTrain);
+    offsetDegrees = DriveTrain.fixDegrees(offsetDegrees);
+    if (offsetDegrees > 180) {
+      offsetDegrees = offsetDegrees - 360;
+    }
     this.offsetDegrees = offsetDegrees;
-    maxTime = 2.5 * (offsetDegrees / 180);
+    maxTime = 2.5 * (Math.abs(offsetDegrees) / 180);
     // give at least 5 seconds for turning
     if (maxTime < 5) {
       maxTime = 5;
     }
-    isClockwise = offsetDegrees > 0 && offsetDegrees <= 180;
+    isClockwise = offsetDegrees > 0;
   }
 
   // Called just before this Command runs the first time
   protected void initialize() {
     startTime = System.nanoTime();
-    startingRotation = Robot.driveTrain.getYaw();
+    startingRotation = DriveTrain.fixDegrees(Robot.driveTrain.getYaw());
     targetRotation = DriveTrain.fixDegrees(startingRotation + offsetDegrees);
     logger.debug(String.format("Start %f, target %f", startingRotation, targetRotation));
+    
     succeeded = false;
     ended = false;
     Robot.driveTrain.shift(DriveTrain.ShiftType.DOWN);
@@ -73,9 +78,18 @@ public class TurnCommand extends AccessibleCommand {
   
   // Called repeatedly when this Command is scheduled to run
   protected void execute() {
+    double localTargetRotation = targetRotation;
+    double currentRotation = DriveTrain.fixDegrees(Robot.driveTrain.getYaw());
+    
+    if (localTargetRotation < currentRotation) {
+      localTargetRotation += 360;
+    }
+
+    isClockwise = localTargetRotation - currentRotation <= 180;
+    
     // We could be turning clockwise or counterclockwise, so an "error" of 350deg for example, is
     // actually an error of 10deg
-    error1 = Math.abs(targetRotation - Robot.driveTrain.getYaw());
+    error1 = Math.abs(targetRotation - currentRotation);
     error2 = Math.abs(360 - error1);
     double actualError = Math.min(error1, error2);
     if (actualError <= autoTurnRampZone) {
