777c03015f41c89af1cdd3c62e7539f75598472c
LaserMonkeySpaceChampion <marknazzaro2@gmail.com>
2017-06-23 19:39:32 -0400

    dfgdfg

--- src/org/ligerbots/steamworks/commands/TankTurnCommand.java
+++ src/org/ligerbots/steamworks/commands/TankTurnCommand.java
@@ -18,7 +18,7 @@ public class TankTurnCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        intialDegrees = Robot.driveTrain.fixDegrees(Robot.driveTrain.getYaw());
+        initialDegrees = Robot.driveTrain.fixDegrees(Robot.driveTrain.getYaw());
     }
 
     // Called repeatedly when this Command is scheduled to run
